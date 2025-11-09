b51cc7069d115510a660e1375117a14cc9581cb0
Swati Tanu <swatitani01@gmail.com>
2025-11-09 22:28:09 +0530

    globalThis

--- Season1/2..JsServer/Notes.md
+++
@@ -1,2 +0,0 @@
-## Server
--- System kept remotely
--- Season1/3.LetUsWriteCode/app.js
+++ Season1/3.LetUsWriteCode/app.js
@@ -10,10 +10,10 @@ console.log(c);
 //! Global object is not a part of V8 engine. In NodeJS's case it is given by NodeJS and in browser's case given by browsers.
 // console.log("Global Object", global)
 
-//? Here "this" (this is an empty object here) is not equal to global unlike this === window in browser. 
+//* Here "this" (this is an empty object here) is not equal to global unlike this === window in browser. 
 
-// Over the time there were a lot of different terms global object, e.g., window, this, self, frames, etc. 
+//* Over the time there were a lot of different terms global object, e.g., window, this, self, frames, etc. 
 
-//? So now, globalThis is a standard way of accessing the global this value acroos environment. Therefore, globalThis === global 
+//* So now, globalThis is a standard way of accessing the global this value acroos environment. Therefore, globalThis === global 
 // console.log(globalThis);
 // console.log(globalThis === global);
