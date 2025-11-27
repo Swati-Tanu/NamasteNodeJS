daff23c2b11e70e567cca4f0e238d61452cfb100
Swati Tanu <swatitani01@gmail.com>
2025-11-27 21:44:33 +0530

    node-github-repo

--- Season1/5.DivingIntoNodeJSGithubRepo/app.js
+++ Season1/5.DivingIntoNodeJSGithubRepo/app.js
@@ -1,12 +1,12 @@
 //require("/path")
 
-// All the code of the module is wrapped inside the function(IIFE)
+//! All the code of the module is wrapped inside the function(IIFE)
 
-//IIFE - Imediately Invoked Function Expression
+//* IIFE - Imediately Invoked Function Expression
 
 // (function () {
-    //All the code of the module is runs here
-    // Keeps variable and function of the module private/safe
+    //? All the code of the module is runs here
+    //* Keeps variable and function of the module private/safe
 // })();
 
 
