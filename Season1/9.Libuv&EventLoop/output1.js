3413f48311bc43f58965f976011db0f0c2a875d7
Swati Tanu <swatitani01@gmail.com>
2025-11-30 22:49:22 +0530

    Event Loop Mechanism

--- Season1/9.Libuv&EventLoop/output1.js
+++ Season1/9.Libuv&EventLoop/output1.js
@@ -1,3 +1,9 @@
+//* Event Loop is a semi-infinite loop.
+//? Because when it has nothing to do it does not keep running but it waits at "Poll Phase".  
+//? As it checks if any API calls (or any poll phase operations) are left to be called.
+//? This is the reason why in our output 3, setImmediate is printed before setInterval.
+//? Other way to put it is if we have some operations inside a particular poll phase operations, then excution starts from "Pool Phase" itself and not from "Timer Phase".
+
 const a = 100;
 
 setImmediate(() => {
